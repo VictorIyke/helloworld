541bce577cf24c56a5518c42a017ff1b8519924e
Victor Iyke-Osuji <106185642+VictorIyke@users.noreply.github.com>
2022-05-24 15:55:27 -0700

    Add files via upload

---
+++ hellowo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << endl << "Type a number" << endl;
+    cin >> num;
+
+    cout << endl << num + 2 << endl;
+
+
+
+    return 0;
+}
